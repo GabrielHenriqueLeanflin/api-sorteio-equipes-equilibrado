553190d9d93681f2587be241cdda85a78b7692e5
Gabriel <gabriel.leanflin@hotmail.com>
2024-09-20 16:24:04 -0300

    feat: Teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel testeeeeeee</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -21,6 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
+            <h1> Testeeeeeeeeeeeeeee API FUNCIONANDO!!! </h1>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
